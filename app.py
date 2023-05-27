fa4ebd44a81a5de82281d0834b0c5488963e92ea
Алексей Тен <virtuoso_ten@mail.ru>
2023-05-27 14:57:24 +0300

    Дописал проект, описал README

--- app.py
+++ app.py
@@ -1,9 +1,56 @@
+import random
+
+from binance.client import Client
+
 from flask import Flask, request, jsonify
 
-from utils import create_orders
-
-
 app = Flask(__name__)
+
+app.config.from_pyfile('secrets.cfg', silent=True)
+
+
+def create_orders(data):
+
+    client = Client(app.config['API_KEY'], app.config['SECRET_KEY'])
+
+    volume = data['volume']
+    number = data['number']
+    amount_dif = data['amountDif']
+    side = data['side']
+    price_min = data['priceMin']
+    price_max = data['priceMax']
+
+    orders = []
+
+    for _ in range(number):
+        price = random.uniform(price_min, price_max)
+
+        amount = random.uniform(
+            volume/number - amount_dif,
+            volume/number + amount_dif
+        )
+
+        if side == 'SELL':
+            order = client.create_order(
+                symbol='BTCUSDT',
+                side='SELL',
+                type='LIMIT',
+                timeInForce='GTC',
+                quantity=amount,
+                price=price
+            )
+        elif side == 'BUY':
+            order = client.create_order(
+                symbol='BTCUSDT',
+                side='BUY',
+                type='LIMIT',
+                timeInForce='GTC',
+                quantity=amount,
+                price=price
+            )
+
+        orders.append(order)
+    return orders
 
 
 @app.route('/create_orders', methods=['POST'])
